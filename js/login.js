cf7a8fd9a4bf1f69de751e4b17b4165ce008e461
Sharafat Rohoman <mdsharafatrohomanzafourllah@example.com>
2025-07-28 00:30:29 +0600

    first commit

---
+++ js/login.js
@@ -0,0 +1,12 @@
+document.getElementById('button-login').addEventListener('click',function(event){
+    event.preventDefault();
+    // console.log('muntaha siddika fariha')
+
+    const phoneNumber = document.getElementById('phone-number').value;
+    console.log(phoneNumber);
+
+
+
+
+
+})
