11c473c31281b1ee3a6f573958d848c67acb1ea7
Sharafat Rohoman <mdsharafatrohomanzafourllah@example.com>
2025-07-28 09:52:16 +0600

    login button added

--- js/login.js
+++ js/login.js
@@ -5,6 +5,17 @@ document.getElementById('button-login').addEventListener('click',function(event)
     const phoneNumber = document.getElementById('phone-number').value;
     console.log(phoneNumber);
 
+    const pinNumber = document.getElementById('pin-number').value;
+    console.log(pinNumber);
+
+    if(phoneNumber === '5' && pinNumber === '1234'){
+        console.log('your are login in');
+        window.location.href = '/home.html'
+    }
+    else{
+        alert('wrong pin number or pin')
+    }
+
 
 
 
